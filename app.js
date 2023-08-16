5b9a4487bfcfa29f02b8117a9c539542840a3021
Boaz Pash <boazpash123@gmail.com>
2023-08-16 16:30:42 +0300

    leevel 7

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const isEmail = require("isemail");
-const crypto = require("crypto");
+const jsonfile = require("jsonfile");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const app = express();
-const users = require("./data");
-
 const port = 8080;
 let uuid = crypto.randomUUID;
 const saltRounds = 10;
+const dataFile = "./data/file.json";
+let users = [];
+
+app.use(cors());
+app.use(express.json());
+
+jsonfile.readFile(dataFile, function (err, obj) {
+  if (err) console.error(err);
+  users = obj;
+});
 
 async function checkUsersPassword(user, pass) {
   const match = await bcrypt.compare(pass, user.password);
   return match;
 }
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
+  console.log(users);
   res.status(200).send(users);
 });
 
@@ -31,21 +39,22 @@ app.get("/users/:id", (req, res) => {
 app.post("/users", async (req, res) => {
   try {
     let userHash = await bcrypt.hash(req.body.password, saltRounds);
-    console.log(userHash);
-    let reqEmail = "";
     if (!isEmail.validate(req.body.email)) {
       res.send("email not valid");
     } else {
-      reqEmail = req.body.email;
+      let reqEmail = req.body.email;
       const newUser = {
         id: uuid(),
         email: reqEmail,
         password: userHash,
       };
       users.push(newUser);
+      jsonfile.writeFile(dataFile, users, function (err) {
+        if (err) console.error(err);
+        console.log(users);
+      });
       res.status(201).send(`${newUser.email} Created successfully`);
     }
-    console.log(users);
   } catch (error) {
     console.log(error);
   }
@@ -59,8 +68,11 @@ app.put("/users/:id", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   };
-  users[users.indexOf(user)] = updatedUser;
-  res.status(200).send(users[users.indexOf(user)]);
+  users[users.indexOf(user[0])] = updatedUser;
+  jsonfile.writeFile(dataFile, users, function (err) {
+    if (err) console.error(err);
+  });
+  res.status(200).send(users[users.indexOf(user[0])]);
   console.log(users);
 });
 
@@ -69,14 +81,17 @@ app.delete("/users/:id", (req, res) => {
   let user = users.filter((item) => item.id == id);
   let index = users[users.indexOf(user)];
   index > 0 ? users.splice(index, index) : users.shift();
-  console.log(users);
+  jsonfile.writeFile(dataFile, users, function (err) {
+    if (err) console.error(err);
+  });
   res.send("User deleted");
 });
 
 app.post("/login", (req, res) => {
-  const user = users.filter((item) => item.email == req.body.email);
+  const user = users.filter((item) => item.email === req.body.email);
   if (user.length === 1) {
-    if (checkUsersPassword(user[0], req.body.password) == true) {
+    // console.log(checkUsersPassword(user[0], req.body.password));
+    if (checkUsersPassword(user[0], req.body.password) === true) {
       res.status(200).send("Connected");
     } else {
       console.log("password incorrect");
--- data.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { id: 1, email: "boazpash@gmail.com", password: "123" },
-  { id: 2, email: "user@gmail.com", password: "456" },
-  { id: 3, email: "user2@gmail.com", password: "789" },
-];
